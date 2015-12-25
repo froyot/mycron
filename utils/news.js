3b17c34978f63787c0e6911c78e122c1dfbb6fc0
Allon <xianlong.wang@dev-engine.com>
2015-12-25 15:58:31 +0800

    edit case error for apicloud module

--- utils/news.js
+++ utils/news.js
@@ -1,8 +1,8 @@
 var superagent = require('superagent');
 var cheerio = require('cheerio');
+var apiUtils = require("./apicloudUtils");
+var client = apiUtils.client;
 
-var Resource=require("APICloud").Resource;
-var client = new Resource("A6966833672975", "2E50D9D3-9063-E9B5-F3B9-065418CF2432");
 
 var news = {};
 var banner_count = 0;
